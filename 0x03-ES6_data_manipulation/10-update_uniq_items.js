457d009713b0e590aab46725fa4603474cce91bb
brysonwaisi <brysonnyamwange@gmail.com>
2023-01-03 21:35:36 +0300

    Map operations

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,7 @@ export default function updateUniqueItems(map) {
   }
   map.forEach((value, key) => {
     if (value === 1) {
-      map.set(key, 100)
+      map.set(key, 100);
     }
-  })
+  });
 }
