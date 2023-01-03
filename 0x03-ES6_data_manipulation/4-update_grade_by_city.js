ffafab4908b25de705dd658d68f5a3d87af7bb3b
brysonwaisi <brysonnyamwange@gmail.com>
2023-01-03 21:44:22 +0300

    Combining map and filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,14 +3,15 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
   if (students instanceof Array) {
     return students
-    .filter((student) => student.location === city)
-    .map((student) => ({
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grade: (newGrades.filter((grade) => grade.studentId === student.id)
-      .pop() || defaultGrade).grade
-    }))
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
   }
   return [];
 }
