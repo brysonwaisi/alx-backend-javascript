333efa42bde8567d5fcfa9982d863943d316587a
brysonwaisi <brysonnyamwange@gmail.com>
2023-01-03 20:24:01 +0300

    Reduce method implementation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,8 @@
-export default function getStudentIds(students) {
+export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce((prevStudent, currStudent) => prevStudent.id || prevStudent + currStudent.id, 0);
+    return students.reduce(
+      (prevStudent, currStudent) => prevStudent.id || prevStudent + currStudent.id,
+      0);
   }
   return 0;
 }
