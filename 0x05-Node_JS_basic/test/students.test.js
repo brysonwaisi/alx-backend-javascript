055f08599a8a8213f6a5fdf87b08719a7d1d5624
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-20 19:35:42 +0300

    Testing with mocha and chai

---
+++ 0x05-Node_JS_basic/test/students.test.js
@@ -0,0 +1,21 @@
+import { use, request, expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../full_server/server';
+
+process.argv[2] = './database.csv';
+
+use(chaiHttp);
+
+describe('server!', () => {
+  it('welcomes user to the api', (done) => {
+    request(app)
+      .get('/students/CS')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equals(
+          'List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie'
+        );
+        done();
+      });
+  });
+});
