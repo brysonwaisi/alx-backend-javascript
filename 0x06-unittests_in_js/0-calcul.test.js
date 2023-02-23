98edd291e93984ef0faf2a0f809f76643998adc7
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-23 15:22:46 +0300

    Tests with  Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,4 @@
 const calculateNumber = require('./0-calcul');
-
 let assert = require('assert');
 
 describe('Tests for calculateNumber', function () {
@@ -13,7 +12,18 @@ describe('Tests for calculateNumber', function () {
     it('should return integer', () => {
       assert.strictEqual(calculateNumber(1, 3.7), 5);
     })
-  })
+  });
+
+  describe("Sums one float & one integer (round down):", function() {
+    it('should return int', function() {
+      assert.strictEqual(calculateNumber(1, 3.3), 4);
+    });
+  });
+  describe("Sums one float & one int reversed:", function () {
+    it('should return int', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+  });
 
   describe('Sums two floating point numbers', () => {
     it('Should return integer', () => {
@@ -21,9 +31,9 @@ describe('Tests for calculateNumber', function () {
     });
   })
 
-  describe('Sums two floating nums and rounds down', () => {
+  describe('Sums two floating nums and with borderline', () => {
     it('Should return integer', () => {
       assert.strictEqual(calculateNumber(1.5, 3.7), 6)
     })
   })
-})
+});
