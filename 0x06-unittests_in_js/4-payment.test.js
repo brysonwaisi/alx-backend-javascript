7664b3913100d3deac4a11fbbcb1e13cb182aa1b
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-25 00:22:43 +0300

    Stubs - Sinon.JS

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,21 +1,21 @@
-const sinon = require("sinon");
-const Utils = require('./utils');
-const { expect } = require('chai');
+const sinon = require('sinon');
+var { expect } = require('chai');
+
 const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
 
+describe('Stubs.', function () {
+  it('Ensures logging message is correct', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');
+    stubUtils.returns(10);
+    const spyConsole = sinon.spy(console, 'log');
 
-describe("Send pay to API", () => {
-  it("should ensure sum output is same", () => {
-    const uncleSam = sinon.spy(console);
-    const breezy = sinon.stub(Utils, 'log');
-    
-    breezy.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(breezy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(breezy.callCount).to.be.equal(1);
-    expect(uncleSam.log.calledWith('The total is: 10')).to.be.true;
-    expect(uncleSam.log.callCount).to.be.equal(1);
-    breezy.restore();
-    uncleSam.log.restore();
+
+    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    stubUtils.restore();
+    spyConsole.restore();
   });
 });
